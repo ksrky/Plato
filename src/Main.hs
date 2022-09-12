97890bce0643a9e3456e6dd99af1e19e60a548c8
ksrky <xgrk9475@gmail.com>
2022-09-12 17:24:01 +0900

    deleted all

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,4 @@
 module Main where
 
-import Plato.Main
-import System.Environment (getArgs)
-
-version :: String
-version = "0.1.0.0"
-
 main :: IO ()
-main = do
-        args <- getArgs
-        case args of
-                [] -> repl
-                "--version" : _ -> putStrLn $ "Plato version " ++ version
-                src : opts -> case opts of
-                        "--debug" : _ -> debugPlato defaultStatus{debug = True, source = src} src
-                        _ -> runPlato defaultStatus{source = src} src
+main = putStrLn "Plato"
