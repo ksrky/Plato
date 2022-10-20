a0ec7d0aac3fb1b1cc0040f77d8f127b2023d28d
ksrky <xgrk9475@gmail.com>
2022-10-20 23:34:43 +0900

    create unit test

---
+++ test/Plato/Test/Utils.hs
@@ -0,0 +1,13 @@
+module Plato.Test.Utils where
+
+import Plato.Parsing.Lexer
+import Plato.Parsing.Monad
+
+import qualified Data.Text as T
+import Test.Hspec
+
+parseLine :: T.Text -> ParserT m a -> m (a, PsState)
+parseLine inp p = parse inp (ParserT $ \st -> runParserT p st{parser_scd = code})
+
+shouldSatisfyReturn :: (HasCallStack, Show a, Eq a) => IO a -> (a -> Bool) -> Expectation
+action `shouldSatisfyReturn` expected = action >>= (`shouldSatisfy` expected)
