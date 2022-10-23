36740bc176c5dcffb66f4e35637339e612982528
ksrky <xgrk9475@gmail.com>
2022-10-24 01:39:47 +0900

    create KindInfer.hs

--- src/Plato/Transl/TypToCore.hs
+++ src/Plato/Transl/TypToCore.hs
@@ -9,17 +9,20 @@ import Plato.Core.Commands as C
 import Plato.Core.Context
 import qualified Plato.Syntax.Core as C
 import qualified Plato.Syntax.Typing as T
+import Plato.Typing.KindInfer
+import Plato.Typing.Renamer
 import Plato.Typing.TrTypes
 
 import Control.Exception.Safe
 import Control.Monad
 import Control.Monad.State
-import Plato.Typing.Renamer
+import qualified Data.Map.Strict as M
+import qualified Data.Vector as V
 
 transExpr = undefined
 
-transType :: MonadThrow m => Context -> T.Type -> m C.Ty
-transType ctx = tratype
+transType :: MonadThrow m => Subst -> Context -> T.Type -> m C.Ty
+transType subst ctx = tratype
     where
         tratype :: MonadThrow m => T.Type -> m C.Ty
         tratype (T.VarT tv) = do
@@ -29,28 +32,36 @@ transType ctx = tratype
                 i <- getVarIndex ctx x
                 return $ C.TyVar i (length ctx)
         tratype (T.ArrT ty1 ty2) = do
-                ty1' <- tratype (unLoc ty1)
-                ty2' <- tratype (unLoc ty2)
-                return $ C.TyArr ty1' ty2'
+                tyT1 <- tratype (unLoc ty1)
+                tyT2 <- tratype (unLoc ty2)
+                return $ C.TyArr tyT1 tyT2
         tratype (T.AllT xs ty) = do
-                let knK1 = C.KnStar -- tmp: forall x.t = forall x::*.t
-                    xs' = map (tyVarName <$>) xs
+                xs' <- forM xs $ \(L sp tv) -> do
+                        knK <- case M.lookup (tyVarName tv) subst of
+                                Just kn -> return $ transKind kn
+                                _ -> throwLocatedErr sp $ "Kind inference failed for " ++ show (tyVarName tv)
+                        return (tyVarName tv, knK)
                 ctx' <- addNames (map (tyVarName <$>) xs) ctx
-                ty' <- transType ctx' (unLoc ty)
-                return $ foldr (`C.TyAll` knK1) ty' (map (tyVarName . unLoc) xs)
+                tyT2 <- transType subst ctx' (unLoc ty)
+                return $ foldr (uncurry C.TyAll) tyT2 xs'
         tratype (T.AbsT x ty) = do
-                let knK1 = C.KnStar -- tmp: \x.t = \x:*.t
+                knK1 <- case M.lookup (unLoc x) subst of
+                        Just knK -> return $ transKind knK
+                        _ -> throwLocatedErr (getSpan x) $ "Kind inference failed for " ++ show x
                 ctx' <- addName x ctx
-                ty' <- transType ctx' (unLoc ty)
-                return $ C.TyAbs (unLoc x) knK1 ty'
+                tyT2 <- transType subst ctx' (unLoc ty)
+                return $ C.TyAbs (unLoc x) knK1 tyT2
         tratype (T.AppT ty1 ty2) = do
-                ty1' <- tratype $ unLoc ty1
-                ty2' <- tratype $ unLoc ty2
-                return $ C.TyApp ty1' ty2'
+                tyT1 <- tratype $ unLoc ty1
+                tyT2 <- tratype $ unLoc ty2
+                return $ C.TyApp tyT1 tyT2
         tratype (T.RecT x ty) = do
+                knK1 <- case M.lookup (unLoc x) subst of
+                        Just knK -> return $ transKind knK
+                        _ -> throwLocatedErr (getSpan x) $ "Kind inference failed for " ++ show x
                 ctx' <- addName x ctx
-                ty' <- transType ctx' (unLoc ty)
-                return $ C.TyRec (unLoc x) C.KnStar ty' -- tmp: unused
+                tyT2 <- transType subst ctx' (unLoc ty)
+                return $ C.TyRec (unLoc x) knK1 tyT2 -- tmp: unused
         tratype (T.RecordT fieldtys) = do
                 fields' <- forM fieldtys $ \(l, field) -> (unLoc l,) <$> tratype (unLoc field)
                 return $ C.TyRecord fields'
@@ -59,19 +70,28 @@ transType ctx = tratype
                 return $ C.TyVariant fields'
         tratype T.MetaT{} = throwUnexpectedErr "Zonking failed"
 
+transKind :: T.Kind -> C.Kind
+transKind T.StarK = C.KnStar
+transKind T.VarK{} = C.KnStar
+transKind (T.ArrK k1 k2) = C.KnArr (transKind k1) (transKind k2)
+
 transDecl :: MonadThrow m => T.Decl -> StateT Context m (Located Name, C.Binding)
 transDecl dec = StateT $ \ctx -> case dec of
         T.TypeD name ty -> do
-                tyT <- transType ctx `traverse` ty
+                (subst, kn) <- inferKind (unLoc ty)
+                let knK = transKind kn
+                tyT <- transType subst ctx `traverse` ty
                 ctx' <- addName name ctx
-                return ((name, C.TyAbbBind tyT Nothing), ctx')
+                return ((name, C.TyAbbBind tyT (Just knK)), ctx')
         T.VarD f ty -> do
-                tyT <- transType ctx `traverse` ty
+                checkKindStar ty
+                tyT <- transType nullSubst ctx `traverse` ty
                 ctx' <- addName f ctx
                 return ((f, C.VarBind tyT), ctx')
         T.FuncD (T.FD f e ty) -> do
+                checkKindStar ty
                 t <- transExpr ctx ty e
-                tyT <- transType ctx `traverse` ty
+                tyT <- transType nullSubst ctx `traverse` ty
                 ctx' <- addName f ctx
                 return ((f, C.TmAbbBind t (Just tyT)), ctx')
 
--- src/Plato/Typing/KindInfer.hs
+++ src/Plato/Typing/KindInfer.hs
@@ -1,42 +1,154 @@
 module Plato.Typing.KindInfer where
 
-{-}
-appKind :: Kind -> Kind -> StateT TypState TypThrow Kind
-appKind k1 k2 = case k1 of
-        ArrKind k11 k12 | k11 == k2 -> return k12
-        ArrKind (VarKind x) k12 -> updateKind x k2 >> return k12
-        VarKind x -> do
-                n <- freshKindName $ unLoc x
-                updateKind x (ArrKind k2 (VarKind n))
-                return $ VarKind n
-        _ -> return StarKind -- error
+import Control.Exception.Safe
+import Control.Monad.State
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+import Plato.Common.Error
+import Plato.Common.Name
+import Plato.Common.Pretty
+import Plato.Common.SrcLoc
+import Plato.Syntax.Typing
+import Plato.Typing.TrTypes
 
-infer :: Located Type -> StateT TypState TypThrow Kind
-infer ty = case unLoc ty of
-        VarType x -> getKind $ unLoc x
-        ArrType ty1 ty2 -> do
-                infer ty1
-                infer ty2
-                return StarKind
-        AllType x _ ty -> do
-                freshKindName $ unLoc x
-                infer ty
-        AbsType x _ ty -> do
-                freshKindName $ unLoc x
-                k2 <- infer ty
-                k1 <- getKind $ unLoc x
-                return $ ArrKind k1 k2
-        AppType ty1 ty2 -> do
-                k2 <- infer ty2
-                k1 <- infer ty1
-                appKind k1 k2
-        RecType x ty -> do
-                freshKindName $ unLoc x
-                infer ty
-        RecordType fields -> do
-                mapM_ (\(_, ty) -> infer ty) fields
-                return StarKind
-        SumType fields -> do
-                forM_ fields $ \(_, tys) -> mapM infer tys
-                return StarKind
--}
+inferKind :: MonadThrow m => Type -> m (Subst, Kind)
+inferKind ty = evalStateT (infer initialEnv ty) initUnique
+
+checkKindStar :: MonadThrow m => Located Type -> m ()
+checkKindStar (L sp ty) = do
+        (_, kn) <- evalStateT (infer initialEnv ty) initUnique
+        when (kn /= StarK) $ throwLocatedErr sp $ show ty ++ " doesn't have Kind *"
+
+-- Context
+newtype KindEnv = KindEnv (M.Map Name Kind)
+
+extend :: KindEnv -> (Name, Kind) -> KindEnv
+extend (KindEnv env) (x, s) = KindEnv $ M.insert x s env
+
+initialEnv :: KindEnv
+initialEnv = KindEnv M.empty
+
+-- Unique number
+newtype Unique = Unique {count :: Int}
+
+initUnique :: Unique
+initUnique = Unique{count = 0}
+
+-- Inference Monad
+type Infer m a = StateT Unique m a
+
+-- Substitution
+type Subst = M.Map Name Kind
+
+nullSubst :: Subst
+nullSubst = M.empty
+
+compose :: Subst -> Subst -> Subst
+s1 `compose` s2 = M.map (apply s1) s2 `M.union` s1
+
+class Substitutable a where
+        apply :: Subst -> a -> a
+        ftv :: a -> S.Set Name
+
+instance Substitutable Kind where
+        apply _ StarK = StarK
+        apply s k@(VarK a) = M.findWithDefault k a s
+        apply s (ArrK t1 t2) = ArrK (apply s t1) (apply s t2)
+        ftv StarK{} = S.empty
+        ftv (VarK a) = S.singleton a
+        ftv (ArrK k1 k2) = ftv k1 `S.union` ftv k2
+
+instance Substitutable a => Substitutable [a] where
+        apply = fmap . apply
+        ftv = foldr (S.union . ftv) S.empty
+
+instance Substitutable KindEnv where
+        apply s (KindEnv env) = KindEnv $ M.map (apply s) env
+        ftv (KindEnv env) = ftv $ M.elems env
+
+-- fresh name generation
+letters :: [String]
+letters = [1 ..] >>= flip replicateM ['k' .. 'z']
+
+fresh :: MonadThrow m => Infer m Kind
+fresh = do
+        s <- get
+        put s{count = count s + 1}
+        return $ VarK $ str2tyvarName (letters !! count s)
+
+-- Unification
+occursCheck :: Substitutable a => Name -> a -> Bool
+occursCheck a t = a `S.member` ftv t
+
+unify :: MonadThrow m => Kind -> Kind -> Infer m Subst
+unify (ArrK l r) (ArrK l' r') = do
+        s1 <- unify l l'
+        s2 <- unify (apply s1 r) (apply s1 r')
+        return (s2 `compose` s1)
+unify (VarK a) k = bind a k
+unify k (VarK a) = bind a k
+unify StarK StarK = return nullSubst
+unify k1 k2 = throwPlainErr $ "UnificationFail " ++ show k1 ++ ", " ++ show k2
+
+bind :: MonadThrow m => Name -> Kind -> Infer m Subst
+bind a k
+        | k == VarK a = return nullSubst
+        | occursCheck a k = throwPlainErr $ "InfiniteKind " ++ show a ++ ", " ++ show k
+        | otherwise = return $ M.singleton a k
+
+-- Inference
+infer :: MonadThrow m => KindEnv -> Type -> Infer m (Subst, Kind)
+infer env t = case t of
+        VarT tv -> lookupEnv env (tyVarName $ unLoc tv)
+        ConT x -> lookupEnv env (unLoc x)
+        ArrT t1 t2 -> do
+                (s1, k1) <- infer env (unLoc t1)
+                (s2, k2) <- infer (apply s1 env) (unLoc t2)
+                s3 <- unify (apply s2 k1) StarK
+                s4 <- unify (apply s3 k2) StarK
+                return (s4 `compose` s3 `compose` s2 `compose` s1, StarK)
+        AllT tvs ty -> do
+                binds <- forM tvs $ \tv -> do
+                        kv <- fresh
+                        return (tyVarName $ unLoc tv, kv)
+                let env' = foldl extend env binds
+                (s1, k1) <- infer env' (unLoc ty)
+                s2 <- unify (apply s1 k1) StarK
+                return (s2 `compose` s1, StarK)
+        AbsT x ty -> do
+                kv <- fresh
+                let env' = env `extend` (unLoc x, kv)
+                (s1, k1) <- infer env' (unLoc ty)
+                return (s1, ArrK (apply s1 kv) k1)
+        AppT t1 t2 -> do
+                kv <- fresh
+                (s1, k1) <- infer env (unLoc t1)
+                (s2, k2) <- infer (apply s1 env) (unLoc t2)
+                s3 <- unify (apply s2 k1) (ArrK k2 kv)
+                return (s3 `compose` s2 `compose` s1, apply s3 kv)
+        RecT x ty -> do
+                kv <- fresh
+                let env' = env `extend` (unLoc x, kv)
+                (s1, k1) <- infer env' (unLoc ty)
+                s2 <- unify (apply s1 k1) StarK
+                return (s2 `compose` s1, StarK)
+        RecordT fields -> do
+                ss <- forM fields $ \(x, ty) -> do
+                        (s1, k1) <- infer env (unLoc ty)
+                        s2 <- unify (apply s1 k1) StarK
+                        return $ s2 `compose` s1
+                return (foldr compose M.empty ss, StarK)
+        SumT fields -> do
+                ss' <- forM fields $ \(x, tys) -> do
+                        ss <- forM tys $ \ty -> do
+                                (s1, k1) <- (infer env . unLoc) ty
+                                s2 <- unify (apply s1 k1) StarK
+                                return $ s2 `compose` s1
+                        return $ foldr compose M.empty ss
+                return (foldr compose M.empty ss', StarK)
+        MetaT{} -> unreachable ""
+
+lookupEnv :: MonadThrow m => KindEnv -> Name -> Infer m (Subst, Kind)
+lookupEnv (KindEnv env) x = case M.lookup x env of
+        Nothing -> throwPlainErr $ "UnboundVariable " ++ show x
+        Just s -> return (nullSubst, s)
