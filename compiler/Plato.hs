1d28487ba5611caacbc600e2906af85d3e855ce9
ksrk <xgrk9475@gmail.com>
2023-10-22 22:19:49 +0900

    feat: apply stylish-haskell on top-level modules

--- compiler/Plato.hs
+++ compiler/Plato.hs
@@ -1,16 +1,16 @@
 module Plato (
-        runPlato,
-        compileToCore,
-        evaluateCore,
-        module Plato.Driver.Flag,
-        module Plato.Driver.Interactive,
-        module Plato.Driver.Monad,
+    runPlato,
+    compileToCore,
+    evaluateCore,
+    module Plato.Driver.Flag,
+    module Plato.Driver.Interactive,
+    module Plato.Driver.Monad,
 ) where
 
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Reader
-import Data.Text qualified as T
+import Data.Text                qualified as T
 
 import Plato.Common.Error
 import Plato.Driver.Context
@@ -20,25 +20,25 @@ import Plato.Driver.Monad
 import Plato.Parsing
 import Plato.PsToTyp
 import Plato.Syntax.Core
-import Plato.TypToCore
 import Plato.Typing
+import Plato.TypToCore
 
 runPlato :: FilePath -> Session -> IO ()
 runPlato filepath = void . unPlato (compileToCore filepath)
 
 compileToCore :: (PlatoMonad m) => FilePath -> m Prog
 compileToCore src = catchErrors $ do
-        pssyn <- parseFile src
-        whenFlagOn FPrintParsed $ liftIO $ printList pssyn
-        typsyn <- psToTyp pssyn
-        typsyn' <- typing typsyn
-        whenFlagOn FPrintTyped $ liftIO $ printList typsyn'
-        corsyn <- typToCore typsyn'
-        whenFlagOn FPrintCore $ liftIO $ printList corsyn
-        return corsyn
+    pssyn <- parseFile src
+    whenFlagOn FPrintParsed $ liftIO $ printList pssyn
+    typsyn <- psToTyp pssyn
+    typsyn' <- typing typsyn
+    whenFlagOn FPrintTyped $ liftIO $ printList typsyn'
+    corsyn <- typToCore typsyn'
+    whenFlagOn FPrintCore $ liftIO $ printList corsyn
+    return corsyn
 
 evaluateCore :: forall m. (PlatoMonad m) => T.Text -> Interactive m ()
 evaluateCore inp = catchErrors $ evalCore =<< lift (compExpr =<< getContext)
-    where
-        compExpr :: Context -> m Term
-        compExpr = runReaderT $ parseExpr inp >>= psToTypExpr >>= typingExpr >>= typToCoreExpr
+  where
+    compExpr :: Context -> m Term
+    compExpr = runReaderT $ parseExpr inp >>= psToTypExpr >>= typingExpr >>= typToCoreExpr
--- compiler/Plato/Typing.hs
+++ compiler/Plato/Typing.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs     #-}
 
 module Plato.Typing (typing, typingExpr) where
 
@@ -7,7 +7,7 @@ import Control.Exception.Safe
 import Control.Monad.IO.Class
 import Control.Monad.Reader
 import Control.Monad.Writer
-import Data.Tuple qualified as Tuple
+import Data.Tuple                 qualified as Tuple
 
 import Plato.Common.Error
 import Plato.Common.Location
@@ -21,22 +21,21 @@ import Plato.Typing.Tc
 import Plato.Typing.Zonking
 
 typingDefns ::
-        (MonadReader e m, HasTypEnv e, HasUniq e, MonadCatch m, MonadIO m) =>
-        [Defn 'Untyped] ->
-        WriterT [Defn 'Typed] m e
+    (MonadReader e m, HasTypEnv e, HasUniq e, MonadCatch m, MonadIO m) =>
+    [Defn 'Untyped] -> WriterT [Defn 'Typed] m e
 typingDefns [] = ask
 typingDefns (ValDefn binds : rest) = do
-        binds' <- zonk =<< tcBinds binds
-        tell [ValDefn binds']
-        local (modifyTypEnv $ extendBinds binds') $ typingDefns rest
+    binds' <- zonk =<< tcBinds binds
+    tell [ValDefn binds']
+    local (modifyTypEnv $ extendBinds binds') $ typingDefns rest
 typingDefns (TypDefn tdefs : rest) = do
-        tdefs' <- zonk =<< kcTypDefns tdefs
-        tell [TypDefn tdefs']
-        let datasig = fmap (\(DatDefn id params _) -> (id, dataSignat params)) tdefs'
-            allctors = (`concatMap` tdefs') $ \(DatDefn _ qns ctors) ->
-                map (\(id, ty) -> (id, L (getLoc ty) $ AllT qns ty)) ctors
-            extconenv env = foldr extendDataBinds env tdefs'
-        local (modifyTypEnv $ extendList allctors . extendList datasig . extconenv) $ typingDefns rest
+    tdefs' <- zonk =<< kcTypDefns tdefs
+    tell [TypDefn tdefs']
+    let datasig = fmap (\(DatDefn id params _) -> (id, dataSignat params)) tdefs'
+        allctors = (`concatMap` tdefs') $ \(DatDefn _ qns ctors) ->
+                        map (\(id, ty) -> (id, L (getLoc ty) $ AllT qns ty)) ctors
+        extconenv env = foldr extendDataBinds env tdefs'
+    local (modifyTypEnv $ extendList allctors . extendList datasig . extconenv) $ typingDefns rest
 
 -----------------------------------------------------------
 -- typing
@@ -45,10 +44,9 @@ typing :: (PlatoMonad m) => Prog 'Untyped -> m (Prog 'Typed)
 typing defs = catchErrors $ updateContext $ Tuple.swap <$> runWriterT (typingDefns defs)
 
 typingExpr ::
-        (MonadReader e m, HasTypEnv e, HasUniq e, MonadCatch m, MonadIO m) =>
-        LExpr 'Untyped ->
-        m (Expr 'Typed)
+    (MonadReader e m, HasTypEnv e, HasUniq e, MonadCatch m, MonadIO m) =>
+    LExpr 'Untyped -> m (Expr 'Typed)
 typingExpr exp = do
-        (exp', ty') <- inferSigma exp
-        checkKindStar =<< zonk (noLoc ty')
-        zonk exp'
+    (exp', ty') <- inferSigma exp
+    checkKindStar =<< zonk (noLoc ty')
+    zonk exp'
